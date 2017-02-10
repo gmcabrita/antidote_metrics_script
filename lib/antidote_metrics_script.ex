85b5ff4ccf800d285dcb35f60904a89966e05c11
Gonçalo Cabrita <_@gmcabrita.com>
2017-02-10 20:53:47 +0000

    oops

--- lib/antidote_metrics_script.ex
+++ lib/antidote_metrics_script.ex
@@ -181,12 +181,12 @@ defmodule AntidoteMetricsScript do
   defp store_metrics(states) do
     {ccrdt_sizes, ccrdt_payloads, crdt_sizes, crdt_payloads} = states
     |> Stream.map(fn(state) ->
-      {ccs, ccp} = Enum.reduce(state.ccrdt_metrics, {0, 0}, fn(m, {size, payload}) ->
-        {size + m.size, payload + m.payload}
+      {ccs, ccp} = Enum.map(state.ccrdt_metrics, fn(m) ->
+        {m.size, m.payload}
       end)
 
-      {cs, cp} = Enum.reduce(state.crdt_metrics, {0, 0}, fn(m, {size, payload}) ->
-        {size + m.size, payload + m.payload}
+      {cs, cp} = Enum.map(state.crdt_metrics, fn(m) ->
+        {m.size, m.payload}
       end)
 
       {ccs, ccp, cs, cp}
